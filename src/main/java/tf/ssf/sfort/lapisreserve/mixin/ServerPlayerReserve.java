65fb5074c4542eaee2aeea17b607d3ae6ddcc7fc
SFort <github@ssf.tf>
2022-06-08 16:07:01 +0200

    update deps

--- src/main/java/tf/ssf/sfort/lapisreserve/mixin/ServerPlayerReserve.java
+++ src/main/java/tf/ssf/sfort/lapisreserve/mixin/ServerPlayerReserve.java
@@ -2,9 +2,11 @@ package tf.ssf.sfort.lapisreserve.mixin;
 
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.network.encryption.PlayerPublicKey;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,9 +15,10 @@ import tf.ssf.sfort.lapisreserve.PlayerInterface;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerReserve extends PlayerEntity {
-	public ServerPlayerReserve(MinecraftServer server, ServerWorld world, GameProfile profile) {
-		super(world, world.getSpawnPos(), world.getSpawnAngle(), profile);
+	public ServerPlayerReserve(World world, BlockPos pos, float yaw, GameProfile gameProfile, @Nullable PlayerPublicKey publicKey) {
+		super(world, pos, yaw, gameProfile, publicKey);
 	}
+
 	@Inject(method = "copyFrom",at=@At("TAIL"))
 	public void copyFrom(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo info) {
 		((PlayerInterface)getInventory()).setLapisreserve(((PlayerInterface)oldPlayer.getInventory()).getLapisreserve());
